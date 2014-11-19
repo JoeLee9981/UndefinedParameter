c1db6d45ccb82dd6eeec98a1e4932f456494c976
JoeLee9981 <joelee19981@gmail.com>
2014-11-19 16:00:07 -0700

    added news resource

---
+++ QuizZing/src/main/java/com/UndefinedParameter/views/NewsArticleView.java
@@ -0,0 +1,19 @@
+package com.UndefinedParameter.views;
+
+import io.dropwizard.views.View;
+
+import com.UndefinedParameter.app.core.NewsArticle;
+
+public class NewsArticleView extends View{
+	
+private final NewsArticle article;
+	
+	public NewsArticleView(NewsArticle article) {
+		super("home.ftl");
+		this.article = article;
+	}
+	
+	public NewsArticle getNews() {
+		return article;
+	}
+}
