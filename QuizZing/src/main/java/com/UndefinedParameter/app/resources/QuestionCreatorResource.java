2e6db490290004ea06ab1633edc696bf032da7e3
JoeLee9981 <joelee19981@gmail.com>
2014-11-26 12:00:31 -0700

    added some more to resource classes

--- QuizZing/src/main/java/com/UndefinedParameter/app/resources/QuestionCreatorResource.java
+++ QuizZing/src/main/java/com/UndefinedParameter/app/resources/QuestionCreatorResource.java
@@ -1,11 +1,16 @@
 package com.UndefinedParameter.app.resources;
 
+import javax.validation.Valid;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import org.eclipse.jetty.http.HttpGenerator.Result;
+
+import com.UndefinedParameter.app.core.Question;
 import com.UndefinedParameter.views.QuestionCreatorView;
 
 @Path("/quiz/create/question")
@@ -17,6 +22,12 @@ public class QuestionCreatorResource {
 	public QuestionCreatorView getQuestionCreatorView() {
 		
 		return new QuestionCreatorView();
+	}
+	
+	@POST
+	public Result creatQuestion(@Valid Question question) {
+		
+		return null;
 	}
 	
 }
