8c060514e9516e4522abfe21737678487658d045
JoeLee9981 <joelee19981@gmail.com>
2015-02-03 14:57:56 -0700

    Changed feedback resource to jdbi

--- QuizZing/src/main/java/com/UndefinedParameter/jdbi/FeedbackDAO.java
+++ QuizZing/src/main/java/com/UndefinedParameter/jdbi/FeedbackDAO.java
@@ -1,96 +1,20 @@
 package com.UndefinedParameter.jdbi;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.Statement;
-import java.util.ArrayList;
+import java.util.List;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.skife.jdbi.v2.sqlobject.Bind;
+import org.skife.jdbi.v2.sqlobject.SqlQuery;
+import org.skife.jdbi.v2.sqlobject.customizers.RegisterMapper;
 
 import com.UndefinedParameter.app.core.Feedback;
-import com.UndefinedParameter.quizzing.QuizZingApplication;
 
-public class FeedbackDAO {
+@RegisterMapper(FeedbackMapper.class)
+public interface FeedbackDAO {
 
-	final static Logger logger = LoggerFactory.getLogger(QuizZingApplication.class);
-	private static final String dbPath = "database/QuizZing";
+	@SqlQuery("INSERT INTO Feedback (feature, improvement, miscellaneous) VALUES(:feature, :improvement, :miscellaneous)")
+	public void addFeedback(@Bind("feature") String feedback, @Bind("improvement") String improvement, @Bind("miscellaneous") String miscellanoues);
 	
-	public static boolean addFeedback(Feedback feedback) {
-		
-		// TODO: Check if the INSERT statement works.
-		int result = -1;
-		String select = "INSERT INTO Feedback (feature, improvement, miscellaneous) VALUES(?, ?, ?)";
-
-		Connection connection = null;
-		PreparedStatement statement = null;
-		
-		try {
-			connection = DriverManager.getConnection("jdbc:sqlite:" + dbPath);
-			connection.setAutoCommit(false);
-			
-			// TODO: Return result == 0 for failure?
-			statement = connection.prepareStatement(select);
-			statement.setString(1, feedback.getSuggestedFeature());
-			statement.setString(2, feedback.getImprovement());
-			statement.setString(3, feedback.getMiscellaneous());
-			
-			result = statement.executeUpdate();
-			connection.commit();
-			
-			statement.close();
-			connection.close();	
-			
-			return true;
-		}
-		catch(Exception e){
-			String errorMsg = "Question could not be inserted into the database. result = " + result; 
-			logger.error(errorMsg);
-			e.printStackTrace();
-		}
-		
-		return false;
-	}
+	@SqlQuery("SELECT * FROM Feedback")
+	public List<Feedback> retrieveFeedback();
 	
-	public static ArrayList<Feedback> retrieveFeedback() {
-		// TODO: Currently retrieves all feedback. Need to
-		// narrow that number down to 100.
-		
-		ArrayList<Feedback> feedbackList = new ArrayList<Feedback>();
-		
-		String select = "SELECT * FROM Feedback";
-		
-		Connection connection = null;
-		Statement statement = null;
-		
-		try {
-			connection = DriverManager.getConnection("jdbc:sqlite:" + dbPath);
-			connection.setAutoCommit(false);
-			
-			statement = connection.createStatement();
-			ResultSet results = statement.executeQuery(select);
-			
-			while(results.next()) {
-				String feature = results.getString("feature");
-				String improve = results.getString("improvement");
-				String misc = results.getString("miscellaneous");
-				Feedback feedback = new Feedback();
-				feedback.setImprovement(improve);
-				feedback.setSuggestedFeature(feature);
-				feedback.setMiscellaneous(misc);
-				feedbackList.add(feedback);
-			}
-			results.close();
-			statement.close();
-			connection.close();
-		}
-		catch(Exception e) {
-			logger.error("Error retrieving feedback from database : " + e.toString());
-			e.printStackTrace();
-		}	
-		
-		return feedbackList;
-	}	
 }
