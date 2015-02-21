248e3714dda61138c9a644dd1b20c33bd6b9a8da
wraith7th <wraith7th@gmail.com>
2015-02-20 21:57:49 -0700

    Added in QuizScore material, basic queries only.

---
+++ QuizZing/src/main/java/com/UndefinedParameter/jdbi/QuizScoreDAO.java
@@ -0,0 +1,32 @@
+package com.UndefinedParameter.jdbi;
+
+import org.skife.jdbi.v2.sqlobject.Bind;
+import org.skife.jdbi.v2.sqlobject.GetGeneratedKeys;
+import org.skife.jdbi.v2.sqlobject.SqlQuery;
+import org.skife.jdbi.v2.sqlobject.SqlUpdate;
+import org.skife.jdbi.v2.sqlobject.customizers.RegisterMapper;
+import org.joda.time.DateTime;
+
+import com.UndefinedParameter.app.core.QuizScore;
+
+@RegisterMapper(UserMapper.class)
+public interface QuizScoreDAO {
+
+	@SqlQuery("SELECT * FROM QuizScore WHERE UserID = :userId")
+	public UserDAO findUserByUserId(@Bind("userId") long userId);
+	
+	@SqlQuery("SELECT * FROM QuizScore WHERE QuizID = :quizid")
+	public UserDAO findUserByQuizID(@Bind("userId") long quizid);
+	
+		
+	@SqlUpdate("INSERT INTO QuizScore "
+			+ "(UserID, QuizID, DateTime, Score) "
+			+ "values "
+			+ "(:userid, :quizid, :datetime, :score)")
+	@GetGeneratedKeys
+	public long insert(@Bind("userid") long userid, 
+					   @Bind("quizid") long quizid,
+					   @Bind("datetime") DateTime datetime,
+					   @Bind("score") float score);
+	
+}
---
+++ QuizZing/src/main/java/com/UndefinedParameter/jdbi/QuizScoreMapper.java
@@ -0,0 +1,37 @@
+package com.UndefinedParameter.jdbi;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.joda.time.DateTime;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
+import org.skife.jdbi.v2.StatementContext;
+import org.skife.jdbi.v2.tweak.ResultSetMapper;
+
+import com.UndefinedParameter.app.core.QuizScore;
+
+public class QuizScoreMapper implements ResultSetMapper<QuizScore> {
+	
+	public QuizScore map(int index, ResultSet r, StatementContext ctx)
+			throws SQLException {
+		
+		QuizScore quizscore = new QuizScore();
+		
+		
+		quizscore.setId(r.getInt("QuizScoreID"));
+		quizscore.setUserID(r.getLong("UserID"));
+		quizscore.setQuizID(r.getLong("QuizID"));
+		
+		//quizscore.setDateTime()
+		DateTimeFormatter formatter = DateTimeFormat.forPattern("dd/MM/yyyy HH:mm:ss");
+		DateTime dt = formatter.parseDateTime(r.getString("DateTime"));
+		quizscore.setDateTime(dt);
+		
+		quizscore.setScore(r.getFloat("Score"));
+		
+
+		return quizscore;
+	}
+
+}
